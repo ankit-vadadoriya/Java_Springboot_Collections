c3a5d4ebc13c089e89dc28713b4de6edc8607c7b
Ankit Vadadoriya <162089228+ankit-vadadoriya@users.noreply.github.com>
2024-10-25 22:18:37 +0200

    upload DemoWithDocker folder

---
+++ 8.DemoWithDocker/RestDemoDocker/src/main/java/com/ankit/RestDemoDocker/RestDemoDockerApplication.java
@@ -0,0 +1,14 @@
+package com.ankit.RestDemoDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoDockerApplication.class, args);
+		System.out.println("Program is running...!");
+	}
+
+}
---
+++ 8.DemoWithDocker/RestDemoDocker/src/test/java/com/ankit/RestDemoDocker/RestDemoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.RestDemoDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
